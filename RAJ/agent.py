114cb69983b40b0c99ff9503f920d993e43ddabf
Raj Barnawal <rjking0568@gmail.com>
2025-08-08 12:27:09 +0530

    Context updated

--- RAJ/agent.py
+++ RAJ/agent.py
@@ -1,17 +1,27 @@
 from google.adk.agents import Agent
-from google.adk.tools import google_search
+# from google.adk.tools import google_search
 
 
+with open('RAJ/prompt.txt', 'r') as file:
+  description = file.read()
 
+instruction = """
+Speak in a friendly, confident, and natural tone — like a real human assistant. 
+Use conversational language, pause naturally between ideas, and sound helpful and approachable. 
+Avoid sounding robotic or overly formal.
 
+Always guide users clearly — if they ask about Vidur's services, explain simply and warmly.
+If you do not know the answer, say so politely and suggest visiting our website or contacting the Vidur team.
 
+Your goal is to make every user feel like they are talking to a real, knowledgeable assistant — powered by Vidur.
+"""
 
 
 root_agent = Agent(
     model='gemini-live-2.5-flash-preview-native-audio',
-    name='google_search_agent',
-    description='A helpful assistant for user question designed by Vidur',
-    instruction='Answer user questions to the best of your knowledge and add that you are designed by Vidur',
-    tools=[google_search],
+    name='vidur_voice_agent',
+    description=description,
+    instruction=instruction,
+    # tools=[google_search],
 )
 
